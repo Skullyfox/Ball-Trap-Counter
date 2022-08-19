8d6b157da964636b62861cf2a9ede51b461e89aa
SanctuaireBot <gxprodpro@gmail.com>
2022-08-19 20:08:17 +0200

    pull-responsive
    missing buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,38 @@
 <template>
   <div class='Container'>
+    <div class='title'>
+      <img src="../assets/logo.png" />
+    </div>
     <div class="playersContainer">
-      <div v-for="(guy,n) in JSON.parse(guys)" 
-        :id="n"
-        :key="n"
-        class="player" 
-      >
+      <div v-for="(guy, n) in JSON.parse(guys)" :id="n" :key="n" class="player">
         <div class="nameContainer">
           <p>{{ guy.name }}</p>
           <button v-on:click="deleteUser(guy.name)">Supprimer</button>
         </div>
         <div class="scoreContainer">
           <span class="destroyed">
-            Touché : {{ guy.destroyed }} 
-            <button v-on:click="updateUser(n,guy.name,guy.destroyed+1,guy.missed)">+</button>
+            Touché : {{ guy.destroyed }}
+            <button v-on:click="updateUser(n, guy.name, guy.destroyed + 1, guy.missed)">+</button>
           </span>
 
           <span class="missed">
-            Loupé : {{ guy.missed }} 
-            <button v-on:click="updateUser(n,guy.name,guy.destroyed,guy.missed+1)">+</button>
+            Loupé : {{ guy.missed }}
+            <button v-on:click="updateUser(n, guy.name, guy.destroyed, guy.missed + 1)">+</button>
           </span>
         </div>
       </div>
     </div>
 
     <div class="optionContainer">
-      <input 
-        v-model="name" 
-        type="text" 
-        v-on:keydown.enter="storeNewUser"
-        v-on:keyup.enter="handleInput($event)"
-      />
-      
-      <button v-on:click="storeNewUser">Ajouter un joueur</button>
-      <button v-on:click="clearScores">Remettre à 0</button>
-      <button v-on:click="newGame">Nouvelle partie</button>
+      <div class="inpute">
+        <input v-model="name" type="text" placeholder="Prénom.." v-on:keydown.enter="storeNewUser"
+          v-on:keyup.enter="handleInput($event)" />
+      </div>
+      <div class="lesbutton">
+        <button class="button" v-on:click="storeNewUser">Ajouter un joueur</button>
+        <button class="button" v-on:click="clearScores">Remettre à 0</button>
+        <button class="button" v-on:click="newGame">Nouvelle partie</button>
+      </div>
     </div>
   </div>
 
@@ -42,66 +40,66 @@
 
 <script>
 export default {
-    name: "IndexPage",
+  name: "IndexPage",
 
-    data() {
-      return {
-        guys: localStorage.getItem('guys'),
-        name: ''
-      }
+  data() {
+    return {
+      guys: localStorage.getItem('guys'),
+      name: ''
+    }
+  },
+
+  mounted() {
+    if (JSON.parse(localStorage.getItem('guys')) === undefined || JSON.parse(localStorage.getItem('guys')) === null) {
+      localStorage.setItem('guys', JSON.stringify([]));
+    }
+  },
+
+  beforeDestroy() { },
+
+  methods: {
+    newGame() {
+      localStorage.setItem('guys', JSON.stringify([]));
+      this.guys = JSON.stringify([]);
     },
-
-    mounted() {
-      if(JSON.parse(localStorage.getItem('guys')) === undefined || JSON.parse(localStorage.getItem('guys')) === null){
-        localStorage.setItem('guys', JSON.stringify([]));
-      }
+    clearScores() {
+      const list = JSON.parse(this.guys);
+      const newList = list.map(player => {
+        player = { name: player.name, destroyed: 0, missed: 0 }
+        return player;
+      });
+      localStorage.setItem('guys', JSON.stringify(newList));
+      this.guys = JSON.stringify(newList);
     },
-
-    beforeDestroy() {},
-    
-    methods: {
-      newGame(){
-        localStorage.setItem('guys', JSON.stringify([]));
-        this.guys = JSON.stringify([]);
-      },
-      clearScores(){
-        const list = JSON.parse(this.guys);
-        const newList = list.map(player => {
-          player = {name: player.name, destroyed: 0, missed: 0}
-          return player;
+    handleInput(e) {
+      e.target.value = '';
+      this.name = '';
+    },
+    storeNewUser(e) {
+      if (process.client) {
+        const list = JSON.parse(localStorage.getItem('guys'));
+        list.push({
+          name: this.name,
+          destroyed: 0,
+          missed: 0
         });
-        localStorage.setItem('guys', JSON.stringify(newList));
-        this.guys = JSON.stringify(newList);
-      },
-      handleInput(e) {
-        e.target.value = '';
-        this.name='';
-      },
-      storeNewUser(e){
-        if(process.client) {
-          const list = JSON.parse(localStorage.getItem('guys'));
-          list.push({
-            name: this.name,
-            destroyed: 0,
-            missed : 0
-          });
         localStorage.setItem('guys', JSON.stringify(list))
         this.guys = JSON.stringify(list)
         this.name = ''
-        }
-      },
-      updateUser(n, guyName, dest, miss){
-        const list = JSON.parse(this.guys);
-        list[n] = {name: guyName, destroyed:dest, missed:miss};
-        this.guys = JSON.stringify(list);
-        localStorage.setItem('guys', JSON.stringify(list));
-      },
-      deleteUser(name){
-        const list = JSON.parse(this.guys);
-        const newList = list.filter(guy => guy.name !== name);
-        this.guys = JSON.stringify(newList);
-        localStorage.setItem('guys', JSON.stringify(newList));
       }
     },
+    updateUser(n, guyName, dest, miss) {
+      const list = JSON.parse(this.guys);
+      list[n] = { name: guyName, destroyed: dest, missed: miss };
+      this.guys = JSON.stringify(list);
+      localStorage.setItem('guys', JSON.stringify(list));
+    },
+    deleteUser(name) {
+      const list = JSON.parse(this.guys);
+      const newList = list.filter(guy => guy.name !== name);
+      this.guys = JSON.stringify(newList);
+      localStorage.setItem('guys', JSON.stringify(newList));
+    }
+  },
 }
 </script>
